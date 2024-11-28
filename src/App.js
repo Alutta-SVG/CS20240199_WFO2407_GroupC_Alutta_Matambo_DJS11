1feec3f10c6d975fee1fd7e960af5a564efeebc0
Alutta <Aluttamatambo5@gmail.com>
2024-11-28 16:14:28 +0200

    Updated App.js and added routing for Home & ShowDetailsPage components.

--- src/App.js
+++ src/App.js
@@ -1,29 +1,18 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from './components/Home';
+import Home from './Components/Home';
 import ShowDetailsPage from './components/ShowDetailsPage';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    <Router>
+    <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/show/:id" element={<ShowDetailsPage />} />
+    </Routes>
+</Router>
+);
+};
 
 export default App;
