db649d052070bd917c19b69a36a5128e0241ce8e
Alutta <Aluttamatambo5@gmail.com>
2024-11-28 16:12:10 +0200

    Added imports

--- src/App.js
+++ src/App.js
@@ -1,3 +1,7 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './components/Home';
+import ShowDetailsPage from './components/ShowDetailsPage';
 import logo from './logo.svg';
 import './App.css';
 
